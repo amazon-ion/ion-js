559eff941e537ca6a72630e7f1db5136514b260b
Michael Montgomery <montgome@amazon.com>
2016-11-02 22:28:39 +0000

    Upgrading to TypeScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,24 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       files: [
-        'src/**/*.js',
+        'build/**/*.js',
         'test/**/*.js'
       ]
+    },
+    typescript: {
+      src: {
+        src: ['src/**/*.ts'],
+        dest: 'build/src'
+      },
+      test: {
+        src: ['test/**/*.ts'],
+        dest: 'build/test'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-typescript');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['typescript', 'jshint']);
 };
