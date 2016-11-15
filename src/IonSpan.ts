8c5ed73b5474860d748ab950970ef6cac36281ac
Michael Montgomery <montgome@amazon.com>
2016-11-15 02:02:09 +0000

    Converting IonSpan.ts, starting extraction of constants to IonConstants.ts

--- src/IonSpan.ts
+++ src/IonSpan.ts
@@ -12,62 +12,43 @@
  * language governing permissions and limitations under the License.
  */
 
-// Parser_number
-//
-// Handles parsing the Ion number formats from a text span.
-// including: int, float, decimal, and timestamp
+namespace ION {
+  const SPAN_TYPE_STRING = 0;
+  const SPAN_TYPE_BINARY = 1;
+  const SPAN_TYPE_SUB_FLAG = 2;
+  const SPAN_TYPE_SUB_STRING = SPAN_TYPE_SUB_FLAG | SPAN_TYPE_STRING;
+  const SPAN_TYPE_SUB_BINARY = SPAN_TYPE_SUB_FLAG | SPAN_TYPE_BINARY;
 
-"use strict";
+  const MAX_POS = 1024*1024*1024; // 1 gig 
+  const LINE_FEED = 10;
+  const CARRAIGE_RETURN = 13;
+  const DEBUG_FLAG = true;
 
-var ION;
-if (!ION) {
-  throw {
-    name: "IonError",
-    where: "loading IonSpan.js",
-    msg: "IonSpan.js must follow Ion.js"
-  };
-}
+  abstract class Span {
+    protected readonly _type: number;
 
-ION.makeSpan = ION.makeSpan || (function() 
-{
-  ION.SPAN_TYPE_STRING = 0;
-  ION.SPAN_TYPE_BINARY = 1;
-  ION.SPAN_TYPE_SUB_FLAG = 2;
-  ION.SPAN_TYPE_SUB_STRING = ION.SPAN_TYPE_SUB_FLAG | ION.SPAN_TYPE_STRING;
-  ION.SPAN_TYPE_SUB_BINARY = ION.SPAN_TYPE_SUB_FLAG | ION.SPAN_TYPE_BINARY;
-  
-  var 
-    MAX_POS = 1024*1024*1024, // 1 gig 
-    LINE_FEED =  10,
-    CARRAIGE_RETURN = 13,
-      DEBUG_FLAG = true,
-    error = function() 
-    { 
-      ION.error("span error"); 
-    },
-    validate = function(n) 
-    { 
-      if (DEBUG_FLAG) {
-        if (typeof n !== 'number') error("invalid value in span");
-        if (!isFinite(n)) error("invalid value in span");
-      }
-    };
+    constructor(_type: number) {
+      this._type = _type;
+    }
 
-  // prototype for the string scanner /"subclass"
-  var string_span = 
-  {
-    _type : undefined,
-    _src : undefined,
-    _pos : 0,
-    _start : 0,
-    _limit : 0,
-    _line : 0,
-    _old_line_start : 0,
-    _line_start : 0,
+    static error() {
+      throw new Error("span error");
+    }
 
-    init : function (src, start, len) // Span(string)
-    {
-      this._type = ION.SPAN_TYPE_STRING;
+    protected abstract clone(start: number, len: number): Span;
+  }
+
+  class StringSpan extends Span {
+    private _src : string;
+    private _pos : number;
+    private _start : number;
+    private _limit : number;
+    private _line : number;
+    private _old_line_start : number;
+    private _line_start : number;
+
+    constructor(src: string, start: number, len: number) {
+      super(SPAN_TYPE_STRING);
       this._line = 1;
       this._src = src;
       this._limit = src.length;
@@ -79,35 +60,30 @@ ION.makeSpan = ION.makeSpan || (function()
       }
       this._start = this._pos;
       this._line_start = this._pos;
-      validate(this._start);
-      validate(this._limit);
-      validate(this._pos);
-    },
-    position : function()
-    {
+      this._old_line_start = 0;
+    }
+
+    position() : number {
       return this._pos - this._start;
-    },
-    getRemaining : function()
-    {
+    }
+
+    getRemaining() : number {
       return this._limit - this._pos;
-    },
-    setRemaining : function(r)
-    {
-      validate(r);
+    }
+
+    setRemaining(r: number) {
       this._limit = r + this._pos;
-    },
-    is_empty : function() 
-    {
-      validate(this._pos);
-      validate(this._limit);
+    }
+
+    is_empty() : boolean  {
       return (this._pos >= this._limit);
-    },
-    next : function()
-    {
+    }
+
+    next() {
       var ch;
       if (this.is_empty()) {
         if (this._pos > MAX_POS) {
-          ION.error("span position is out of bounds");
+          throw new Error("span position is out of bounds");
         }
         this._pos++; // we increment this even though we don't use it (this 
                      // should not have other issues since past the end is 
@@ -117,26 +93,27 @@ ION.makeSpan = ION.makeSpan || (function()
       ch = this._src.charCodeAt(this._pos);
       if (ch === CARRAIGE_RETURN) {
         if (this.peek() != LINE_FEED) {
-          this._inc_line(ch);
+          this._inc_line();
         }
       }
       else if (ch == LINE_FEED) {
-        this._inc_line(ch);
+        this._inc_line();
       }
       this._pos++;
       return ch;
-    },
-    _inc_line : function () {
+    }
+
+    _inc_line() {
         this._old_line_start = this._line_start;
         this._line++;
         this._line_start = this._pos;
-    },
-    unread : function(ch)
-    {
-      if (this._pos <= this._start) error();
+    }
+
+    unread(ch) {
+      if (this._pos <= this._start) Span.error();
       this._pos--;
       if (ch < 0) {
-        if (this.is_empty() != true) error();
+        if (this.is_empty() != true) Span.error();
         return;
       }
       // we can only unread across 1 new line
@@ -144,49 +121,51 @@ ION.makeSpan = ION.makeSpan || (function()
           this._line_start = this._old_line_start;
           this._line--;
       }
-      if (ch != this.peek()) error();  // DEBUG
-    },
-    peek : function()
-    {
+      if (ch != this.peek()) Span.error();  // DEBUG
+    }
+
+    peek() {
       if (this.is_empty()) return ION.EOF;
       return this._src.charCodeAt(this._pos);
-    },
-    skip : function(dist) {
-      validate(dist);
+    }
+
+    skip(dist: number): void {
       this._pos += dist;
       if (this._pos > this._limit) {
         this._pos = this._limit;
       }
-    },
-    get : function(ii) {
+    }
+
+    get(ii): number {
       if (ii < this._start || ii >= this._limit) return ION.EOF;
       return this._src.charCodeAt(ii);
-    },
-    line_number : function()
-    {
-      return this._line;
-    },
-    offset : function()
-    {
-      return this._pos - this._line_start;
-    },
-  };
-  var StringSpan = function() {};
-  StringSpan.prototype = string_span;
-  StringSpan.prototype.constructor = StringSpan;
-  
-  // prototype for the binary scanner "subclass"
-  var binary_span = 
-  {
-    _type : undefined,
-    _src : undefined,
-    _pos : 0,
-    _start : 0,
-    _limit : 0,
+    }
 
-    init : function (src, start, len) // Span(byte array)
-    {
-      this._type = ION.SPAN_TYPE_BINARY
+    line_number() : number {
+      return this._line;
+    }
+
+    offset() : number {
+      return this._pos - this._line_start;
+    }
+
+    clone(start: number, len: number) : StringSpan {
+      let actual_len: number = this._limit - this._pos - start;
+      if (actual_len > len) {
+        actual_len = len;
+      }
+      return new StringSpan(this._src, this._pos, actual_len);
+    }
+  }
+
+  class BinarySpan extends Span {
+    private _src: number[];
+    private _pos: number;
+    private _start: number;
+    private _limit: number;
+
+    constructor(src: number[], start: number, len: number) {
+      super(SPAN_TYPE_BINARY);
       this._src = src;
       this._limit = src.length;
       if (typeof start !== 'undefined') {
@@ -196,34 +175,29 @@ ION.makeSpan = ION.makeSpan || (function()
         }
       }
       this._start = this._pos;
-      validate(this._pos);
-      validate(this._limit);
-    },
-    position : function()
-    {
+    }
+
+    position() : number {
       return this._pos - this._start;
-    },
-    getRemaining : function()
-    {
+    }
+
+    getRemaining() : number {
       return this._limit - this._pos;
-    },
-    setRemaining : function(r)
-    {
-      validate(r);
+    }
+
+    setRemaining(r: number) {
       this._limit = r + this._pos;
-    },
-    is_empty : function() 
-    {
-      validate(this._pos);
-      validate(this._limit);
+    }
+
+    is_empty(): boolean {
       return (this._pos >= this._limit);
-    },
-    next : function()
-    {
+    }
+
+    next(): number {
       var b;
       if (this.is_empty()) {
         if (this._pos > MAX_POS) {
-          ION.error("span position is out of bounds");
+          throw new Error("span position is out of bounds");
         }
         this._pos++;
         return ION.EOF;
@@ -231,78 +205,59 @@ ION.makeSpan = ION.makeSpan || (function()
       b = this._src[this._pos];
       this._pos++;
       return (b & 0xFF);
-    },
-    unread : function(b)
-    {
-      if (this._pos <= this._start) error();
+    }
+
+    unread(b: number) {
+      if (this._pos <= this._start) Span.error();
       this._pos--;
       if (b == ION.EOF) {
-        if (this.is_empty() == false) error();
+        if (this.is_empty() == false) Span.error();
       }
-      if (b != this.peek()) error();    // DEBUG
-    },
-    peek : function()
-    {
+      if (b != this.peek()) Span.error();    // DEBUG
+    }
+
+    peek(): number {
       if (this.is_empty()) return ION.EOF;
       return (this._src[this._pos] & 0xFF);
-    },
-    skip : function(dist) {
-      validate(dist);
+    }
+
+    skip(dist: number) {
       this._pos += dist;
       if (this._pos > this._limit) {
         this._pos = this._limit;
       }
-    },
-    get : function(ii) {
+    }
+
+    get(ii: number) {
       if (ii < this._start || ii >= this._limit) return undefined;
       return (this._src[ii] & 0xFF);
-    },
-  };
-  var BinarySpan = function() {};
-  BinarySpan.prototype = binary_span;
-  BinarySpan.prototype.constructor = BinarySpan;
-
-  // make this into a "class", it's a constructor who's prototype is the parser above
-  // and we make the prototype parser's constructor this constructor function
-  var makeSpan_impl = function (src, start, len) {
-    var span = undefined,
-        src_type = typeof src
-    ;
-    if (src_type === 'undefined') error();
-    if (src_type === 'string') {
-      span = new StringSpan();
-      span.init(src, start, len);
     }
-    else if (src_type === 'object') {
-      if (typeof (src.isSpan) === 'undefined') { // probably an array
-        span = new BinarySpan();
-        span.init(src, start, len);
+
+    clone(start: number, len: number) : BinarySpan {
+      let actual_len: number = this._limit - this._pos - start;
+      if (actual_len > len) {
+        actual_len = len;
       }
-      else if (arg.isSpan) {
-        var actual_len = src._limit - src._pos - start;
-        if (actual_len > len) actual_len = len;
-        switch(this._type) {
-        case ION.SPAN_TYPE_STRING:
-        case ION.SPAN_TYPE_SUB_STRING:
-          span = new StringSpan();
-          span.init(src._src, src._pos, actual_len);
-          break;
-        case ION.SPAN_TYPE_BINARY:
-        case ION.SPAN_TYPE_SUB_BINARY:
-          span = new BinarySpan();
-          span.init(src._src, src._pos + start, actual_len);
-          break;
-        default:
-          break;
-        }
+      return new BinarySpan(this._src, this._pos + start, actual_len);
+    }
+  }
+
+  function makeSpan_impl(src, start: number, len: number): Span {
+    let span: Span = undefined;
+    let src_type = typeof src;
+
+    if (src_type === 'undefined') {
+      Span.error();
+    } else if (src_type === 'string') {
+      span = new StringSpan(src, start, len);
+    } else if (src_type === 'object') {
+      if (typeof (src.isSpan) === 'undefined') { // probably an array
+        span = new BinarySpan(src, start, len);
       }
     }
     if (span === undefined) {
-      ION.error("invalid span source");
+      throw new Error("invalid span source");
     }
     return span;
   };
-
-  return makeSpan_impl;
 }
-)(); // and we call the closure
