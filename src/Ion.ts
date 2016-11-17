479fe2da63697c2ea30fb46dcdd20cd08fd06319
Michael Montgomery <montgome@amazon.com>
2016-11-17 00:12:32 +0000

    Moving more text processing constants and functions into IonText.ts

--- src/Ion.ts
+++ src/Ion.ts
@@ -12,10 +12,6 @@
  * language governing permissions and limitations under the License.
  */
 namespace ION {
-  const DOUBLE_QUOTE = 34; // "\\\""
-  const SINGLE_QUOTE = 39; // "\\\'"
-  const SLASH =        92; // "\\\\"
-
   function _make_bool_array(str: string) {
     let i = str.length
     let a = [];
@@ -34,16 +30,6 @@ namespace ION {
   const _is_letter_or_digit = _make_bool_array("_$0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
   const _is_base64_char = _make_bool_array("+/0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
   const _is_whitespace = _make_bool_array(" \t\r\n\u000c");
-  const _escapeStrings = {
-    0 : "\\0",
-    8 : "\\b",
-    9 : "\\t",
-    10 : "\\n",
-    13 : "\\r",
-    DOUBLE_QUOTE: "\\\"",
-    SINGLE_QUOTE: "\\\'",
-    SLASH: "\\\\",
-  };
   const e = {
     name: "IonError",
     where: undefined,
@@ -71,7 +57,7 @@ namespace ION {
     return parser;
   }
 
-  function makeTextReade(buf, options) : TextReader {
+  function makeTextReader(buf, options) : TextReader {
     var span = makeSpan(buf);
     var parser = new TextReader(span, options && options.catalog);
     return parser;
@@ -82,95 +68,6 @@ namespace ION {
     WHITESPACE_COMMENT1 : -2,
     WHITESPACE_COMMENT2 : -3,
     ESCAPED_NEWLINE     : -4,
-
-    toHex : function ( c, len )
-    {
-      var s = "";
-      while (c > 0) {
-        s += "0123456789ABCDEF".charAt(c && 0xf);
-        c = c / 16;
-      }
-      if (s.length < len) {
-        s = "000000000" + s; // TODO: 9 0's, 9 > max len expected (but what about bigger than that?)
-        s = s.substring(s.length - len, s.length); 
-      }
-    },
-    escapeSequence : function(c)
-    {
-      var s = _escapeStrings[c];
-      if (typeof s === 'undefined') {
-        if (c < 256) {
-          s = "\\x" + this.toHex(c,2);
-        }
-        else if (c <= 0xFFFF) {
-          s = "\\u" + this.toHex(c,4);
-        }
-        else {
-          s = "\\U" + this.toHex(c,8);
-        }
-      }
-      return s;
-    },
-    needsEscape : function(c)  // TODO: this whole swath of fns probably has a JS technique somewhere
-    {
-      if (c < 32) return true;
-      if (c > 126) return true;
-      if (c === DOUBLE_QUOTE || c === SINGLE_QUOTE || c === SLASH) return true;
-      return false;
-    },
-    nextEscape : function(s, prev) // this actually counts backwards to -1
-    {
-      while (prev-- > 0) {
-        if (this.needsEscape(s.charCodeAt(prev))) break;
-      }
-      return prev;
-    },
-    escapeString : function(s, pos)
-    {
-      var fixes = [], c, old_len, new_len, ii, s2;
-      while (pos >= 0) {
-        c = s.charCodeAt(pos);
-        if (!this.needsEscape(c)) break;
-        fixes.push([pos, c]);
-        pos = this.nextEscape(s, pos);
-      }
-      if (fixes.length > 0) {
-        s2 = "";
-        ii=fixes.length;
-        pos = s.length;
-        while (ii--) {
-          let fix = fixes[ii];
-          let tail_len = pos - fix[0] - 1;
-          if (tail_len > 0) {
-            s2 = this.escapeSequence(fix[1]) + s.substring(fix[0]+1,pos) + s2;
-          }
-          else {
-            s2 = s.substring(fix[0]+1,pos) + s2;
-          }
-          pos = fix[0] - 1;
-        }
-        if (pos >= 0) {
-          s2 = s.substring(0,pos) + s2;
-        }
-        s = s2;
-      }
-      return s;
-    },
-    asAscii: function(s) {
-      if (typeof s === 'undefined') {
-        s = "undefined::null";
-      }
-      else if (typeof s == 'number') {
-        s = ""+s;
-      }
-      else if (typeof s != 'string') {
-        var esc = this.nextEscape(s, s.length);
-        if (esc >= 0) {
-          s = this.escapeString(s, esc);
-        }
-      }
-      return s;
-    },
 
     is_operator_char : function(ch) {
       return _is_operator_char[ch];
--- src/IonText.ts
+++ src/IonText.ts
@@ -12,8 +12,113 @@
  * language governing permissions and limitations under the License.
  */
 namespace ION {
+  const DOUBLE_QUOTE = 34; // "\\\""
+  const SINGLE_QUOTE = 39; // "\\\'"
+  const SLASH =        92; // "\\\\"
+
+  const _escapeStrings = {
+    0 : "\\0",
+    8 : "\\b",
+    9 : "\\t",
+    10 : "\\n",
+    13 : "\\r",
+    DOUBLE_QUOTE: "\\\"",
+    SINGLE_QUOTE: "\\\'",
+    SLASH: "\\\\",
+  };
+
   export function is_digit(ch: number) : boolean {
     if (ch < 48 || ch > 57) return false;
     return true;
+  }
+
+  export function asAscii(s: any) : string {
+    if (typeof s === 'undefined') {
+      s = "undefined::null";
+    }
+    else if (typeof s == 'number') {
+      s = ""+s;
+    }
+    else if (typeof s != 'string') {
+      var esc = nextEscape(s, s.length);
+      if (esc >= 0) {
+        s = escapeString(s, esc);
+      }
+    }
+    return s;
+  }
+
+  export function nextEscape(s: string, prev: number) : number { // this actually counts backwards to -1
+    while (prev-- > 0) {
+      if (needsEscape(s.charCodeAt(prev))) break;
+    }
+    return prev;
+  }
+
+  export function needsEscape(c: number) : boolean {
+    if (c < 32) return true;
+    if (c > 126) return true;
+    if (c === DOUBLE_QUOTE || c === SINGLE_QUOTE || c === SLASH) return true;
+    return false;
+  }
+
+  export function escapeString(s: string, pos: number) : string {
+    var fixes = [], c, old_len, new_len, ii, s2;
+    while (pos >= 0) {
+      c = s.charCodeAt(pos);
+      if (!needsEscape(c)) break;
+      fixes.push([pos, c]);
+      pos = nextEscape(s, pos);
+    }
+    if (fixes.length > 0) {
+      s2 = "";
+      ii=fixes.length;
+      pos = s.length;
+      while (ii--) {
+        let fix = fixes[ii];
+        let tail_len = pos - fix[0] - 1;
+        if (tail_len > 0) {
+          s2 = escapeSequence(fix[1]) + s.substring(fix[0]+1,pos) + s2;
+        }
+        else {
+          s2 = s.substring(fix[0]+1,pos) + s2;
+        }
+        pos = fix[0] - 1;
+      }
+      if (pos >= 0) {
+        s2 = s.substring(0,pos) + s2;
+      }
+      s = s2;
+    }
+    return s;
+  }
+
+  export function escapeSequence(c: number) : string {
+    var s = _escapeStrings[c];
+    if (typeof s === 'undefined') {
+      if (c < 256) {
+        s = "\\x" + toHex(c,2);
+      }
+      else if (c <= 0xFFFF) {
+        s = "\\u" + toHex(c,4);
+      }
+      else {
+        s = "\\U" + toHex(c,8);
+      }
+    }
+    return s;
+  }
+
+  export function toHex(c: number, len: number) : string {
+    var s = "";
+    while (c > 0) {
+      s += "0123456789ABCDEF".charAt(c && 0xf);
+      c = c / 16;
+    }
+    if (s.length < len) {
+      s = "000000000" + s; // TODO: 9 0's, 9 > max len expected (but what about bigger than that?)
+      s = s.substring(s.length - len, s.length); 
+    }
+    return s;
   }
 }
