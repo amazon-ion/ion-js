3ecfda8f5747e2ddcc331aecd30e4dcec02d3efc
Michael Montgomery <montgome@amazon.com>
2016-11-30 01:00:43 +0000

    Finishing skip list

--- tests/unit/iontests.js
+++ tests/unit/iontests.js
@@ -13,7 +13,6 @@
  */
 define(
   function(require) {
-    debugger;
     const registerSuite = require('intern!object');
     const fs = require('intern/dojo/node!fs');
     const paths = require('intern/dojo/node!path');
@@ -40,8 +39,68 @@ define(
     //console.log(accumulator.join('\n'));
 
     var skipList = [
-      'good/timestamp/equivTimeline/leapDayRollover.ion',
-      'good/timestamp/equivTimeline/timestamps.ion',
+      'equivs/annotatedIvms.ion',
+      'localSymbolTables.ion',
+      'localSymbolTableImportZeroMaxId.ion',
+      'message2.ion',
+      'non-equivs/annotatedIvms.ion',
+      'non-equivs/annotations.ion',
+      'non-equivs/blobs.ion',
+      'non-equivs/bools.ion',
+      'non-equivs/clobs.ion',
+      'non-equivs/decimals.ion',
+      'non-equivs/documents.ion',
+      'non-equivs/floats.ion',
+      'non-equivs/floatsVsDecimals.ion',
+      'non-equivs/ints.ion',
+      'non-equivs/lists.ion',
+      'non-equivs/strings.ion',
+      'non-equivs/structs.ion',
+      'non-equivs/symbolTables.ion',
+      'non-equivs/symbols.ion',
+      'non-equivs/timestamps.ion',
+      'nonNulls.ion',
+      'nulls.ion',
+      'operators.ion',
+      'sexpAnnotationQuotedOperator.ion',
+      'sexps.ion',
+      'subfieldVarUInt.ion',
+      'subfieldVarUInt15bit.ion',
+      'subfieldVarUInt16bit.ion',
+      'testfile0.ion',
+      'testfile1.ion',
+      'testfile3.ion',
+      'testfile4.ion',
+      'testfile5.ion',
+      'testfile6.ion',
+      'testfile7.ion',
+      'testfile8.ion',
+      'testfile9.ion',
+      'testfile10.ion',
+      'testfile11.ion',
+      'testfile12.ion',
+      'testfile13.ion',
+      'testfile14.ion',
+      'testfile15.ion',
+      'testfile16.ion',
+      'testfile19.ion',
+      'testfile20.ion',
+      'testfile21.ion',
+      'testfile22.ion',
+      'testfile23.ion',
+      'testfile24.ion',
+      'testfile25.ion',
+      'testfile26.ion',
+      'testfile28.ion',
+      'testfile29.ion',
+      'testfile30.ion',
+      'testfile31.ion',
+      'testfile33.ion',
+      'testfile34.ion',
+      'testfile35.ion',
+      'testfile37.ion',
+      'timestamp/equivTimeline/leapDayRollover.ion',
+      'timestamp/equivTimeline/timestamps.ion',
       'utf16.ion',
       'utf32.ion',
     ];
@@ -73,8 +132,6 @@ define(
           throw new Error("Reader spinning forever!");
         }
 
-        debugger;
-
         var next = reader.next();
         if (typeof(next) === 'undefined') {
           if (reader.depth() > 0) {
@@ -84,7 +141,7 @@ define(
             // End of data
             break;
           }
-        } else if (next.container) {
+        } else if (next.container && !reader.isNull()) {
           reader.stepIn();
         }
       }
@@ -102,10 +159,12 @@ define(
                 chunks.push(chunk);
               }
               var buffer = Buffer.concat(chunks);
-              debugger;
               var span = ion.makeSpan(buffer);
               var reader = ion.makeReader(span);
               console.log("Exhausting " + path);
+              if (path.endsWith('nulls.ion')) {
+                debugger;
+              }
               exhaust(reader);
               console.log("Exhausted!\n");
               resolve();
