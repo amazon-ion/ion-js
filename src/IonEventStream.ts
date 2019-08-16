96c2f2f118e7e60d75bf9f0e78d73e051d858afe
Wesboyt <wesboyt@amazon.com>
2019-08-16 13:43:12 -0700

    removed extraneous newline.

--- src/IonEventStream.ts
+++ src/IonEventStream.ts
@@ -139,7 +139,6 @@ export class IonEventStream {
                 }
                 case IonEventType.CONTAINER_START : {
                     if (actualEvent.equals(expectedEvent)) {
-
                         actualIndex = actualIndex + actualEvent.ionValue.length;
                         expectedIndex = expectedIndex + expectedEvent.ionValue.length;
                     } else {
