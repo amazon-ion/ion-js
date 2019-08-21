42d14cd62bfc67bb8703e4402793fc738a9bb79f
Peter Cornell <pcornell@amazon.com>
2019-08-21 16:35:03 -0700

    Decimal API cleanup (#313)
    Decimal API changes:
    * renames getExponent() to _getExponent()
    * renames getDigits() to _getCoefficient()
    * removes isZero(), isNegativeZero(), isZeroZero(), getNumber(), stringValue(), isNull(), lessThan(), greaterThan(), leq(), geq()
    * renames ZERO to _ZERO
    * implements toString() based on the algorithm implemented by java.math.BigDecimal.toString()
    
    Other changes:
    * moves Decimal.stringValue() logic to IonTextWriter.writeDecimal()
    * removes LongInt.digits()
    * re-adds previously skipped decimal unit tests

--- src/IonTextWriter.ts
+++ src/IonTextWriter.ts
@@ -11,8 +11,8 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 * language governing permissions and limitations under the License.
 */
-import { Decimal } from "./IonDecimal";
-import { encodeUtf8 } from "./IonUnicode";
+import {Decimal} from "./IonDecimal";
+import {encodeUtf8} from "./IonUnicode";
 import {
     escape,
     toBase64,
@@ -24,10 +24,11 @@ import {
     ClobEscapes,
     is_keyword
 } from "./IonText";
-import { Timestamp } from "./IonTimestamp";
-import { TypeCodes } from "./IonBinary";
-import { Writeable } from "./IonWriteable";
-import { Writer } from "./IonWriter";
+import {Timestamp} from "./IonTimestamp";
+import {TypeCodes} from "./IonBinary";
+import {Writeable} from "./IonWriteable";
+import {Writer} from "./IonWriter";
+import {_sign} from "./util";
 
 type Serializer<T> = (value: T) => void;
 
@@ -105,7 +106,23 @@ export class TextWriter implements Writer {
 
     writeDecimal(value: Decimal, annotations?: string[]) : void {
         this.writeValue(TypeCodes.DECIMAL, value, annotations, (value: Decimal) => {
-            this.writeUtf8(value.toString());
+            if (value === null) {
+                this.writeUtf8("null.decimal");
+            } else {
+                let s = '';
+                let coefficient = value._getCoefficient();
+                if (coefficient.isZero() && coefficient.signum() === -1) {
+                    s += '-';
+                }
+                s += coefficient.toString() + 'd';
+
+                let exponent = value._getExponent();
+                if (exponent === 0 && _sign(exponent) === -1) {
+                    s += '-';
+                }
+                s += exponent;
+                this.writeUtf8(s);
+            }
         });
     }
 
