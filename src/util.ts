d636e3fd8e7fdf4339f8dba4f00b36e81f492ca5
Peter Cornell <pcornell@amazon.com>
2019-08-27 09:35:52 -0700

    adds equivs and non-equivs testing (#323)

--- src/util.ts
+++ src/util.ts
@@ -35,37 +35,48 @@ export function _writeValues(reader: IonReader, writer: IonWriter, _depth = 0): 
         type = reader.next();
     }
     while (type !== null) {
-        if (_depth > 0) {
-            if (reader.fieldName() != null) {
-                writer.writeFieldName(reader.fieldName());
-            }
-        }
-        if (reader.isNull()) {
-            writer.writeNull(type.bid, reader.annotations());
-        } else {
-            switch (type) {
-                case IonTypes.BOOL:      writer.writeBoolean(reader.booleanValue(), reader.annotations()); break;
-                case IonTypes.INT:       writer.writeInt(reader.numberValue(), reader.annotations()); break;
-                case IonTypes.FLOAT:     writer.writeFloat64(reader.numberValue(), reader.annotations()); break;
-                case IonTypes.DECIMAL:   writer.writeDecimal(reader.decimalValue(), reader.annotations()); break;
-                case IonTypes.TIMESTAMP: writer.writeTimestamp(reader.timestampValue(), reader.annotations()); break;
-                case IonTypes.SYMBOL:    writer.writeSymbol(reader.stringValue(), reader.annotations()); break;
-                case IonTypes.STRING:    writer.writeString(reader.stringValue(), reader.annotations()); break;
-                case IonTypes.CLOB:      writer.writeClob(reader.byteValue(), reader.annotations()); break;
-                case IonTypes.BLOB:      writer.writeBlob(reader.byteValue(), reader.annotations()); break;
-                case IonTypes.LIST:      writer.writeList(reader.annotations()); break;
-                case IonTypes.SEXP:      writer.writeSexp(reader.annotations()); break;
-                case IonTypes.STRUCT:    writer.writeStruct(reader.annotations()); break;
-                default: throw new Error('Unrecognized type ' + (type !== null ? type.name : type));
-            }
-            if (type.container) {
-                reader.stepIn();
-                _writeValues(reader, writer, _depth + 1);
-                writer.endContainer();
-                reader.stepOut();
-            }
-        }
+        _writeValue(reader, writer, _depth);
         type = reader.next();
+    }
+}
+
+/**
+ * Writes the current value from a reader.
+ */
+export function _writeValue(reader: IonReader, writer: IonWriter, _depth = 0): void {
+    let type: IonType = reader.type();
+    if (type === null) {
+        return;
+    }
+    if (_depth > 0) {
+        if (reader.fieldName() != null) {
+            writer.writeFieldName(reader.fieldName());
+        }
+    }
+    if (reader.isNull()) {
+        writer.writeNull(type.bid, reader.annotations());
+    } else {
+        switch (type) {
+            case IonTypes.BOOL:      writer.writeBoolean(reader.booleanValue(), reader.annotations()); break;
+            case IonTypes.INT:       writer.writeInt(reader.numberValue(), reader.annotations()); break;
+            case IonTypes.FLOAT:     writer.writeFloat64(reader.numberValue(), reader.annotations()); break;
+            case IonTypes.DECIMAL:   writer.writeDecimal(reader.decimalValue(), reader.annotations()); break;
+            case IonTypes.TIMESTAMP: writer.writeTimestamp(reader.timestampValue(), reader.annotations()); break;
+            case IonTypes.SYMBOL:    writer.writeSymbol(reader.stringValue(), reader.annotations()); break;
+            case IonTypes.STRING:    writer.writeString(reader.stringValue(), reader.annotations()); break;
+            case IonTypes.CLOB:      writer.writeClob(reader.byteValue(), reader.annotations()); break;
+            case IonTypes.BLOB:      writer.writeBlob(reader.byteValue(), reader.annotations()); break;
+            case IonTypes.LIST:      writer.writeList(reader.annotations()); break;
+            case IonTypes.SEXP:      writer.writeSexp(reader.annotations()); break;
+            case IonTypes.STRUCT:    writer.writeStruct(reader.annotations()); break;
+            default: throw new Error('Unrecognized type ' + (type !== null ? type.name : type));
+        }
+        if (type.container) {
+            reader.stepIn();
+            _writeValues(reader, writer, _depth + 1);
+            writer.endContainer();
+            reader.stepOut();
+        }
     }
 }
 
