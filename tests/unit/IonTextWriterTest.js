e74ca2dfe8eff38622a89e812d16846e342c92ad
Wesboyt <wesboyt@amazon.com>
2019-09-27 14:14:43 -0700

    removes toplevel calls, removes toplevel method, adds public depth method to writer interface and writer implementations, adds depth() unit tests.

--- tests/unit/IonTextWriterTest.js
+++ tests/unit/IonTextWriterTest.js
@@ -29,7 +29,7 @@
               var writeable = new ionTest.Writeable();
               var writer = new ionTextWriter.TextWriter(writeable);
               instructions(writer);
-              while(!writer.isTopLevel){
+              while(writer.depth() > 0){
                   writer.stepOut();
               }
               writer.close();
@@ -44,7 +44,7 @@
           suite[name] = function() {
               let writer = new ion.makePrettyWriter(2);
               instructions(writer);
-              while(!writer.isTopLevel){
+              while(writer.depth() > 0){
                   writer.stepOut();
               }
               writer.close();
@@ -543,6 +543,37 @@
     a26::null.null
   )
 }`);
+
+      //depth tests
+      var depthTest = function(name, instructions, expectedDepth) {
+          suite[name] = function() {
+              var writeable = new ionTest.Writeable();
+              var writer = new ionTextWriter.TextWriter(writeable);
+              instructions(writer);
+              assert.equal(writer.depth(), expectedDepth);
+          }
+      }
+      depthTest('Stepping into a list results in a depth of 1.',
+          (writer) => {writer.stepIn(ion.IonTypes.LIST);}, 1);
+      depthTest('Stepping into a struct results in a depth of 1.',
+          (writer) => {writer.stepIn(ion.IonTypes.STRUCT);}, 1);
+      depthTest('Stepping into a sexp results in a depth of 1.',
+          (writer) => {writer.stepIn(ion.IonTypes.SEXP);}, 1);
+      depthTest('Stepping into 2 lists results in a depth of 2.',
+          (writer) => {writer.stepIn(ion.IonTypes.LIST); writer.stepIn(ion.IonTypes.LIST);}, 2);
+      depthTest('Stepping into 2 lists and out results in a depth of 1.',
+          (writer) => {
+              writer.stepIn(ion.IonTypes.LIST);
+              writer.stepIn(ion.IonTypes.LIST);
+              writer.stepOut()},
+          1);
+      depthTest('Stepping into a list results in a depth of 1.',
+          (writer) => {
+              writer.stepIn(ion.IonTypes.LIST);
+              writer.stepIn(ion.IonTypes.LIST);
+              writer.stepOut(),
+                  writer.stepIn(ion.IonTypes.SEXP);},
+          2);
 
     registerSuite(suite);
   }
