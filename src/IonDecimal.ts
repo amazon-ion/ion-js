5335be1b4032001bc116605b44fc41a54e826b04
Wesboyt <wesboyt@amazon.com>
2019-09-30 16:22:27 -0700

    fixes numberValue() bug which will fry on any bigints requiring over 53 bits.

--- src/IonDecimal.ts
+++ src/IonDecimal.ts
@@ -105,7 +105,11 @@ export class Decimal {
      */
     toString(): string {
         // based on the algorithm defined in https://docs.oracle.com/javase/8/docs/api/java/math/BigDecimal.html#toString--
-        let cStr = Math.abs(this._coefficient.numberValue())+'';
+        let cStr = this._coefficient.toString();
+        let negative = cStr[0] === '-';
+        if(negative) {
+            cStr = cStr.substr(1, cStr.length);
+        }
         let precision = cStr.length;
         let adjustedExponent = this._exponent + (precision - 1);
 
