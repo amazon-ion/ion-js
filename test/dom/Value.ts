6be79767807f6879a9cda4112bdc08a5e6594226
Zack Slayton <zslayton@amazon.com>
2020-03-13 12:41:04 -0400

    Added dumpBinary(), dumpText(), dumpPrettyText().

--- test/dom/Value.ts
+++ test/dom/Value.ts
@@ -6,21 +6,11 @@ import JSBI from "jsbi";
 import * as JsValueConversion from "../../src/dom/JsValueConversion";
 import {_hasValue} from "../../src/util";
 import {Constructor} from "../../src/dom/Value";
+import {exampleIonValuesWhere, exampleJsValuesWhere, valueName} from "../../src/IonTests";
 
 // The same test logic performed by assert.equals() but without an assertion.
 function jsEqualsIon(jsValue, ionValue): boolean {
     return jsValue == ionValue;
-}
-
-// Generates a mocha-friendly string representation of the provided value that can be used in test names
-function valueName(value: any): string {
-    if (value === null) {
-        return 'null';
-    }
-    if (typeof value === "object") {
-        return `${value.constructor.name}(${value})`;
-    }
-    return `${typeof value}(${value})`;
 }
 
 // Tests whether each value is or is not an instance of dom.Value
@@ -38,47 +28,6 @@ function instanceOfValueSubclassTest(constructor: Constructor, expected: boolean
             assert.equal(value instanceof constructor, expected);
         });
     }
-}
-
-// Used as a default filter on the `exampleJsValuesWhere` and `exampleIonValuesWhere` functions below.
-function acceptAnyValue(_: any) : boolean {
-    return true;
-}
-
-// A common collection of JS values that can be reduced to a relevant subset using the provided filter function.
-function exampleJsValuesWhere(filter: (v: any) => boolean = acceptAnyValue): any[] {
-    return [
-        null,
-        true, false,
-        Number.MIN_SAFE_INTEGER, -7.5, -7, 0, 7, 7.5, Number.MAX_SAFE_INTEGER,
-        "", "foo",
-        new Date(0),
-        Timestamp.parse('1970-01-01T00:00:00Z'),
-        new Decimal('1.5'),
-        [], [1, 2, 3], [{foo: "bar"}],
-        {}, {foo: "bar", baz: 5}, {foo: [1, 2, 3]}
-    ].filter(filter);
-}
-
-// A common collection of Ion dom.Value instances that can be reduced to a relevant subset using
-// the provided filter function.
-function exampleIonValuesWhere(filter: (v: Value) => boolean = acceptAnyValue): Value[] {
-    return [
-        load('null')!, // null
-        load('null.string')!, // typed null
-        load('true')!, // boolean
-        load('1')!, // integer
-        load('15e-1')!, // float
-        load('15d-1')!, // decimal
-        load('1970-01-01T00:00:00.000Z')!, // timestamp
-        load('"Hello"')!, // string
-        load('Hello')!, // symbol
-        load('{{aGVsbG8gd29ybGQ=}}')!, // blob
-        load('{{"February"}}')!, // clob
-        load('[1, 2, 3]')!, // list
-        load('(1 2 3)')!, // s-expression
-        load('{foo: true, bar: "Hello", baz: 5, qux: null}')! // struct
-    ].filter(filter);
 }
 
 // Describes the static side of dom.Value subclasses
---
+++ test/dump.ts
@@ -0,0 +1,84 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import {assert} from 'chai';
+import * as ion from '../src/IonTests';
+import {dom, exampleIonValuesWhere, exampleJsValuesWhere, IonTypes} from "../src/IonTests";
+
+function roundTripTests(...values: any[]) {
+    let expectedValues: dom.Value[] = values.map((jsValue) => dom.Value.from(jsValue));
+    it(`dumpBinary(${values})`, () => roundTripTest(ion.dumpBinary, values, expectedValues));
+    it(`dumpText(${values})`,() => roundTripTest(ion.dumpText, values, expectedValues));
+    it(`dumpPrettyText(${values})`, () => roundTripTest(ion.dumpPrettyText, values, expectedValues));
+}
+
+// Dumps the provided values to Ion using the specified dump*() function, uses loadAll() to read the values
+// back in, then compares the input values to the loaded values.
+function roundTripTest(dumpFn: (...values: any[]) => any,
+                       inputValues: any[],
+                       expectedValues: dom.Value[]) {
+    let actualValues: dom.Value[] = dom.loadAll(dumpFn(inputValues));
+    let testComparisons: [dom.Value, dom.Value][] = actualValues.map(
+        (actualValue, index) => [actualValue, expectedValues[index]]
+    );
+    for (let [actualValue, expectedValue] of testComparisons) {
+        //TODO: This tests for a very weak common definition of equality.
+        //      We should make this a stronger test when .equals() is added to the Value interface[1].
+        //      In the meantime, the core writing/equality logic is more strictly tested by
+        //      test/dom/Value.ts, which evaluates the data produced by the Value.writeTo() function
+        //      at the core of the ion.dump*() methods.
+        //      [1] https://github.com/amzn/ion-js/issues/576
+        assert.deepEqual(actualValue.getAnnotations(), expectedValue.getAnnotations());
+        assert.equal(actualValue.isNull(), expectedValue.isNull());
+        assert.equal(actualValue.getType(), expectedValue.getType());
+    }
+}
+
+describe('dump*()', () => {
+    describe('Round tripping JS values', () => {
+        roundTripTests(exampleJsValuesWhere())
+    });
+    describe('Round tripping Ion values', () => {
+        roundTripTests(exampleIonValuesWhere())
+    });
+    it('Text stream', () => {
+        let ionText = ion.dumpText(null, 7, "Hello", [1, 2, 3]);
+        assert.equal(ionText, 'null\n7\n"Hello"\n[1,2,3]');
+    });
+    it('Pretty text stream', () => {
+        let ionPrettyText = ion.dumpPrettyText(null, 7, "Hello", [1, 2, 3]);
+        assert.equal(ionPrettyText, 'null\n7\n"Hello"\n[\n  1,\n  2,\n  3\n]');
+    });
+    it('Binary stream', () => {
+        let ionBinary: Uint8Array = ion.dumpBinary(null, 7, "Hello", [1, 2, 3]);
+        let writer = ion.makeBinaryWriter();
+        writer.writeNull(IonTypes.NULL);
+        writer.writeInt(7);
+        writer.writeString("Hello");
+        writer.stepIn(IonTypes.LIST);
+        writer.writeInt(1);
+        writer.writeInt(2);
+        writer.writeInt(3);
+        writer.stepOut();
+        writer.close();
+        let expectedBytes: Uint8Array = writer.getBytes();
+        let byteComparisons: [number, number][] = Array.from(ionBinary).map(
+            (byte, index) => [byte, expectedBytes[index]]
+        );
+        for (let [actualByte, expectedByte] of byteComparisons) {
+            assert.equal(actualByte, expectedByte);
+        }
+    });
+});
