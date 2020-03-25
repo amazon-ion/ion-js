2335a25820702e10ebe8339afec51c10b9e91f0b
Zack Slayton <zack.slayton@gmail.com>
2020-03-25 13:32:34 -0400

    JSON.stringify() support for dom.Clob (#591)

--- src/dom/Clob.ts
+++ src/dom/Clob.ts
@@ -20,4 +20,26 @@ export class Clob extends Lob(IonTypes.CLOB) {
         writer.setAnnotations(this.getAnnotations());
         writer.writeClob(this);
     }
+
+    toJSON() {
+        // Because the text encoding of the bytes stored in this Clob is unknown,
+        // we write each byte out as a Unicode escape (e.g. 127 -> 0x7f -> \u007f)
+        // unless it happens to fall within the ASCII range.
+        // See the Ion cookbook's guide to down-converting to JSON for details:
+        // http://amzn.github.io/ion-docs/guides/cookbook.html#down-converting-to-json
+        let encodedText = '';
+        for (let byte of this) {
+            if (byte >= 32 && byte <= 126) {
+                encodedText += String.fromCharCode(byte);
+                continue;
+            }
+            let hex = byte.toString(16);
+            if (hex.length == 1) {
+                encodedText += "\\u000" + hex;
+            } else {
+                encodedText += "\\u00" + hex;
+            }
+        }
+        return encodedText;
+    }
 }
