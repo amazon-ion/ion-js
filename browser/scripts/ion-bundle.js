7fceb4374885848803903ce32889c9664a0c9615
Khushboo Desai <desaikd@amazon.com>
2023-09-07 13:25:46 -0700

    adds documentation for v5.2.0

--- browser/scripts/ion-bundle.js
+++ browser/scripts/ion-bundle.js
@@ -423,15 +423,15 @@ function isBinary(buffer) {
   }
   return true;
 }
-function makeReader(buf) {
+function makeReader(buf, catalog) {
   if (typeof buf === "string") {
-    return new IonTextReader_1.TextReader(new IonSpan_1.StringSpan(buf));
+    return new IonTextReader_1.TextReader(new IonSpan_1.StringSpan(buf), catalog);
   }
   var bufArray = new Uint8Array(buf);
   if (isBinary(bufArray)) {
-    return new IonBinaryReader_1.BinaryReader(new IonSpan_1.BinarySpan(bufArray));
+    return new IonBinaryReader_1.BinaryReader(new IonSpan_1.BinarySpan(bufArray), catalog);
   } else {
-    return new IonTextReader_1.TextReader(new IonSpan_1.StringSpan(IonUnicode_1.decodeUtf8(bufArray)));
+    return new IonTextReader_1.TextReader(new IonSpan_1.StringSpan(IonUnicode_1.decodeUtf8(bufArray)), catalog);
   }
 }
 exports.makeReader = makeReader;
@@ -7542,8 +7542,8 @@ var Timestamp = /*#__PURE__*/function () {
       var fractionStr = "";
       if (exp < 0) {
         var idx = Math.max(coefStr.length + exp, 0);
-        secondsStr = coefStr.substr(0, idx);
-        fractionStr = coefStr.substr(idx);
+        secondsStr = coefStr.substring(0, idx);
+        fractionStr = coefStr.substring(idx);
         if (-secondsDecimal.getExponent() - coefStr.length > 0) {
           fractionStr = "0".repeat(-exp - coefStr.length) + fractionStr;
         }
@@ -11951,31 +11951,31 @@ function _iterableToArray(iter) {
 }
 module.exports = _iterableToArray, module.exports.__esModule = true, module.exports["default"] = module.exports;
 },{}],72:[function(require,module,exports){
-function _iterableToArrayLimit(arr, i) {
-  var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"];
-  if (null != _i) {
-    var _s,
-      _e,
-      _x,
-      _r,
-      _arr = [],
-      _n = !0,
-      _d = !1;
+function _iterableToArrayLimit(r, l) {
+  var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"];
+  if (null != t) {
+    var e,
+      n,
+      i,
+      u,
+      a = [],
+      f = !0,
+      o = !1;
     try {
-      if (_x = (_i = _i.call(arr)).next, 0 === i) {
-        if (Object(_i) !== _i) return;
-        _n = !1;
-      } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0);
-    } catch (err) {
-      _d = !0, _e = err;
+      if (i = (t = t.call(r)).next, 0 === l) {
+        if (Object(t) !== t) return;
+        f = !1;
+      } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0);
+    } catch (r) {
+      o = !0, n = r;
     } finally {
       try {
-        if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return;
+        if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return;
       } finally {
-        if (_d) throw _e;
+        if (o) throw n;
       }
     }
-    return _arr;
+    return a;
   }
 }
 module.exports = _iterableToArrayLimit, module.exports.__esModule = true, module.exports["default"] = module.exports;
@@ -12060,14 +12060,14 @@ function _toPropertyKey(arg) {
 }
 module.exports = _toPropertyKey, module.exports.__esModule = true, module.exports["default"] = module.exports;
 },{"./toPrimitive.js":80,"./typeof.js":82}],82:[function(require,module,exports){
-function _typeof(obj) {
+function _typeof(o) {
   "@babel/helpers - typeof";
 
-  return (module.exports = _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) {
-    return typeof obj;
-  } : function (obj) {
-    return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-  }, module.exports.__esModule = true, module.exports["default"] = module.exports), _typeof(obj);
+  return (module.exports = _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) {
+    return typeof o;
+  } : function (o) {
+    return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o;
+  }, module.exports.__esModule = true, module.exports["default"] = module.exports), _typeof(o);
 }
 module.exports = _typeof, module.exports.__esModule = true, module.exports["default"] = module.exports;
 },{}],83:[function(require,module,exports){
